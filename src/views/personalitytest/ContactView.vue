4fe24a0448e7aff63b6062e1ffb797f7a9bdf77f
王智 <1848195475@qq.com>
2024-11-13 17:33:17 +0800

    demo

--- src/views/personalitytest/ContactView.vue
+++ src/views/personalitytest/ContactView.vue
@@ -1,32 +1,76 @@
 <template>
-    <button @click="sendRequest">点击发送信息到网址</button>
-    <p>返回的数据：{{ responseData }}</p>
+  <div class="psychological-test">
+    <h1>心理小测试</h1>
+    <div v-if="currentQuestion < questions.length">
+      <h2>{{ questions[currentQuestion].question }}</h2>
+      <div v-for="(answer, index) in questions[currentQuestion].answers" :key="index">
+        <button @click="selectAnswer(index)">{{ answer }}</button>
+      </div>
+    </div>
+    <div v-else>
+      <h2>测试结果</h2>
+      <p>{{ result }}</p>
+      <button @click="restartTest">重新测试</button>
+    </div>
+  </div>
 </template>
-<script setup lang="ts">
-import { useConfig } from '@/composables/config';
-import { computed } from 'vue';
-import { ref } from "vue";
-import axios from 'axios';
 
-const responseData = ref();
-
-const sendRequest = async () => {
-  try {
-    // 使用 POST 请求发送数据到指定网址
-    const response = await axios.post("http://localhost:9000/myHello/greet2", {
-      // 要发送的数据，根据需求修改
-      name: "value2",
-    });
-    console.log("请求：", response);
-    // 将返回的数据存储到响应式变量中
-    responseData.value = response.data;
-  } catch (error) {
-    console.error("请求失败：", error);
-  }
+<script>
+export default {
+  data() {
+    return {
+      currentQuestion: 0,
+      answers: [],
+      questions: [
+        {
+          question: "你最喜欢的季节是？",
+          answers: ["春天", "夏天", "秋天", "冬天"],
+        },
+        {
+          question: "你的理想职业是什么？",
+          answers: ["医生", "老师", "工程师", "艺术家"],
+        },
+        {
+          question: "你对待困难的态度是？",
+          answers: ["勇敢面对", "逃避", "寻求帮助", "找到其他解决办法"],
+        },
+      ],
+    };
+  },
+  computed: {
+    result() {
+      return this.answers.length ? `你的选择是：${this.answers.join(', ')}` : '';
+    },
+  },
+  methods: {
+    selectAnswer(index) {
+      this.answers.push(this.questions[this.currentQuestion].answers[index]);
+      this.currentQuestion++;
+    },
+    restartTest() {
+      this.currentQuestion = 0;
+      this.answers = [];
+    },
+  },
 };
-const pageSize = 10
-const config = useConfig()
-const allFaqs = computed(() => config.contact.faqs)
-const page = computed(() => Math.ceil(allFaqs.value.length / pageSize))
-const data = computed(() => allFaqs.value.slice((page.value - 1) * pageSize, page.value * pageSize))
 </script>
+
+<style scoped>
+.psychological-test {
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+
+button {
+  margin: 5px;
+  padding: 10px 15px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+</style>

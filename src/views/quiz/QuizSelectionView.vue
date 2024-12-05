35110a636b84708d1edd3820693d5a932298f61d
王智 <1848195475@qq.com>
2024-12-05 12:25:24 +0800

    根据是否有测试结果显示重新测试/开始测试

--- src/views/quiz/QuizSelectionView.vue
+++ src/views/quiz/QuizSelectionView.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="container">
-        <div class="test-list">
-            <div class="test-item" v-for="(item, index) in quizNames">
-                <h2>{{ item.name }}</h2>
-                <button @click="enterTest(quizNames[index].id)">进入测试</button>
-            </div>
+    <div class="test-list">
+        <div class="test-item" v-for="(item, index) in quizNames">
+            <h2>{{ item.name }}</h2>
+            <button @click="enterTest(quizNames[index].id)">{{ buttonText[index] }}</button>
         </div>
     </div>
 </template>
@@ -13,43 +11,53 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import router from '@/router';
+import { useUserStore } from '@/store/userStore';
 
+const username = useUserStore().username;
 const quizNames = ref<{ name: string; id: number }[]>([]);
+const buttonText = ref<string[]>(new Array(quizNames.value.length));
 
-const getQuizNames = async () => {
+const getQuizs = async () => {
     const res = await axios.post('http://localhost:9000/quiz/getQuizNames');
     quizNames.value = res.data;
+    if (username !== "" && username !== undefined) {
+        for (let i = 0; i < quizNames.value.length; i++) {
+            const res = await axios.post('http://localhost:9000/quiz/getQuizResult', {
+                username: username,
+                quizId: quizNames.value[i].id
+            });
+            buttonText.value[i] = res.data ? '重新测试' : '开始测验';
+        }
+    }
+    else
+        buttonText.value = buttonText.value.fill("开始测验(游客)");
 }
 
 const enterTest = (id: number) => {
     router.push({ name: 'quiz', params: { id: id } });
 }
 
-// 在组件挂载时获取测验名称
+// 在组件挂载时获取测验名称和按钮文字
 onMounted(() => {
-    getQuizNames();
+    getQuizs();
 });
 </script>
 
 <style scoped>
-.container {
-    max-width: 800px;
-    margin: auto;
-    text-align: center;
-}
-
 .test-list {
     display: flex;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
     justify-content: center;
+    margin: auto;
+    text-align: center;
 }
 
 .test-item {
     margin: 20px;
     border: 1px solid #ccc;
     border-radius: 10px;
-    padding: 10px;
-    width: 200px;
+    padding: 100px;
+    max-width: 800px;
 }
 
 button {
@@ -59,6 +67,7 @@ button {
     border: none;
     border-radius: 5px;
     cursor: pointer;
+    justify-self: right;
 }
 
 button:hover {

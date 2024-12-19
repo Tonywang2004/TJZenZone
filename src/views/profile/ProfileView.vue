64c620cf1e4f37a69aef2d0b7786c575817f75f8
王智 <1848195475@qq.com>
2024-12-19 21:59:06 +0800

    图表修复、布局重构

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,49 +1,55 @@
 <template>
   <div class="profile-view">
-    <h1>个人资料</h1>
-    <ol class="profile-list">
-      <li>
-        <h3>用户名</h3>
-        <p>{{ userStore.username }}</p>
-      </li>
-      <li>
-        <h3>邮箱</h3>
-        <p>{{ email }}</p>
-      </li>
-      <li>
-        <h3>人格类型</h3>
-        <p>{{ mbti }}</p>
-      </li>
-    </ol>
-    <!-- 添加柱状图的容器 -->
-    <div ref="gameChart" style="width: 600px; height: 400px; justify-self: center;"></div>
-    <div ref="whitenoiseChart" style="width: 600px; height: 400px; justify-self: center;"></div>
+    <div class="profile-info">
+      <h3>个人资料</h3>
+      <ol class="profile-list">
+        <li>
+          <h4>用户名</h4>
+          <p>{{ userStore.username }}</p>
+        </li>
+        <li>
+          <h4>邮箱</h4>
+          <p>{{ email }}</p>
+        </li>
+        <li>
+          <h4>人格类型</h4>
+          <p>{{ mbti }}</p>
+        </li>
+      </ol>
+    </div>
+    <div class="charts">
+      <div v-if="gamesContent" class="chart-message">
+        <div id="gameChart" style="width: 100%; height: 100%;"/>
+      </div>
+      <div v-else class="chart-message">许多有趣的游戏等您探索！</div>
+      <div v-if="whitenoiseContent" class="chart-message">
+        <div id="whitenoiseChart" style="width: 100%; height: 100%;"/>
+      </div>
+      <div v-else class="chart-message">许多白噪声等您探索！</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 import {useUserStore} from '@/store/userStore';
 import axios from 'axios';
-import {onMounted, ref} from 'vue';
+import {onMounted, ref, nextTick, watch} from 'vue';
 import * as echarts from 'echarts';
 
 const userStore = useUserStore();
 
-interface gameState {
-  gameType: string,
-  highScore: number,
-  duration: number
-}
+let email = ref<string>('登录可以查看更多内容！');
+let mbti = ref<string>('登录可以查看更多内容！');
 
-let email = ref<string>('您未登录！');
-let mbti = ref<string>('您未登录！');
 let whitenoises = ref<Map<string, number>>(new Map());
-let games = ref<Array<gameState>>([]);
-let gameChart = ref<HTMLDivElement | null>(null);
-let whitenoiseChart = ref<HTMLDivElement | null>(null);
+let gametypes = ref<Array<string>>([]);
+let gamescores = ref<Array<number>>([]);
+let gamedurations = ref<Array<number>>([]);
 
-onMounted(async () => {
+let gamesContent = ref<boolean>(false);
+let whitenoiseContent = ref<boolean>(false);
+
+const getData = (async () => {
   const emailRes = await axios.post('http://localhost:9000/profile/email', {username: userStore.username});
   email.value = emailRes.data;
   const mbtiRes = await axios.post('http://localhost:9000/profile/mbti', {username: userStore.username});
@@ -52,94 +58,110 @@ onMounted(async () => {
   whitenoises.value = new Map(Object.entries(wnRes.data));
   const gameRes = await axios.post('http://localhost:9000/profile/game', {username: userStore.username});
   for (const game of gameRes.data) { // 字段名需与后端一致
-    const type = game['gameType'];
-    const score = game['highestScore'];
-    const duration = game['gameDuration'];
-    games.value.push({gameType: type, highScore: score, duration: duration});
+    gametypes.value.push(game['gameType']);
+    gamescores.value.push(game['highestScore']);
+    gamedurations.value.push(game['duration']);
   }
+  gamesContent.value = gametypes.value.length > 0;
+  whitenoiseContent.value = whitenoises.value.size > 0;
+});
 
-  if (gameChart.value) {
-    if (games.value.length === 0) {
-      gameChart.value.style.display = 'none';
-      gameChart.value.style.width = '0px';
-      gameChart.value.style.height = '0px';
-    }
-    else{
-      const myChart = echarts.init(gameChart.value);
-      const gametypes = games.value.map(game => game.gameType);
-      const gamedurations = games.value.map(game => game.duration);
-      const gamescores = games.value.map(game => game.highScore);
-      // 配置项
-      const option = {
-        title: {
-          text: '游戏游玩数据'
-        },
-        tooltip: {},
-        legend: {
-          data: ['时长', '最高分']
-        },
-        xAxis: {
-          data: gametypes
-        },
-        yAxis: {},
-        series: [{
-          name: '时长',
-          type: 'bar',
-          data: gamedurations
-        }, {
-          name: '最高分',
-          type: 'bar',
-          data: gamescores
-        }]
-      };
-      myChart.setOption(option);
-    }
+const initGameChart = (()=>{
+  if (document.getElementById("gameChart")){
+    echarts.init(document.getElementById("gameChart")).setOption({
+      title: {
+        text: '游戏游玩数据'
+      },
+      tooltip: {},
+      legend: {
+        data: ['时长', '最高分']
+      },
+      xAxis: {
+        data: gametypes.value
+      },
+      yAxis: {},
+      series: [{
+        name: '时长',
+        type: 'bar',
+        data: gamedurations.value
+      }, {
+        name: '最高分',
+        type: 'bar',
+        data: gamescores.value
+      }]
+    });
   }
+});
 
-  if (whitenoiseChart.value) {
-    const myChart = echarts.init(whitenoiseChart.value);
-    if (whitenoises.value.size === 0) {
-      whitenoiseChart.value.style.display = 'none';
-      whitenoiseChart.value.style.width = '0px';
-      whitenoiseChart.value.style.height = '0px';
-    }
-    else{
-      // 配置项
-      const option = {
-        title: {
-          text: '白噪声数据'
-        },
-        tooltip: {},
-        legend: {
-          data: ['时长']
-        },
-        xAxis: {
-          data: Array.from(whitenoises.value.keys())
-        },
-        yAxis: {},
-        series: [{
-          name: '时长',
-          type: 'bar',
-          data: Array.from(whitenoises.value.values())
-        }]
-      };
-      myChart.setOption(option);
-    }
+const initWhiteNoiseChart =(()=>{
+  if (document.getElementById("whitenoiseChart")) {
+    echarts.init(document.getElementById("whitenoiseChart")).setOption({
+      title: {
+        text: '白噪声数据'
+      },
+      tooltip: {},
+      legend: {
+        data: ['时长']
+      },
+      xAxis: {
+        data: Array.from(whitenoises.value.keys())
+      },
+      yAxis: {},
+      series: [{
+        name: '时长',
+        type: 'bar',
+        data: Array.from(whitenoises.value.values())
+      }]
+    });
+  }
+})
+
+onMounted(async () => {
+  await getData();
+  await nextTick();
+  initGameChart();
+  initWhiteNoiseChart();
+});
+
+watch(gamesContent, async (newValue) => {
+  if (newValue) {
+    await nextTick(); // 确保 DOM 已经更新
+    initGameChart();
+  }
+});
+
+watch(whitenoiseContent, async (newValue) => {
+  if (newValue) {
+    await nextTick(); // 确保 DOM 已经更新
+    initWhiteNoiseChart();
   }
 });
 </script>
 
 <style scoped>
 .profile-view {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
   text-align: center;
   font-family: 'Arial', sans-serif;
   padding: 20px;
 }
 
+.profile-info {
+  width: 50%;
+  text-align: left;
+}
+
+.charts {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .profile-list {
-  text-align: center;
   list-style: none;
-  justify-content: center;
   padding: 0;
   /* 清除默认内边距 */
 }
@@ -158,21 +180,30 @@ onMounted(async () => {
   /* 鼠标悬停时的背景颜色 */
 }
 
-h1 {
-  font-size: 2em;
-  /* 增大标题字体 */
-  color: #333;
-  /* 修改标题颜色 */
+.chart-message {
+  width: 80%;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 h3 {
+  margin-left: 5%;
+  font-size: 2em;
+  /* 增大标题字体 */
   color: #007BFF;
+  /* 修改标题颜色 */
+}
+
+h4 {
+  margin-left: 10%;
+  color: #82cbff;
   /* 修改子标题颜色 */
 }
 
 p {
-  font-size: 1.2em;
-  /* 增大段落字体 */
+  margin-left: 15%;
   color: #555;
   /* 修改文本颜色 */
 }
